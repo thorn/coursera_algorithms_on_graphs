e124a8a58ed0fddd9e34a609cdf503c506dd9db5
Arsen Shamkhalov <thornu731@gmail.com>
2016-07-02 20:33:18 +0300

    feat: add solution to the first problem of week 4 dijkstra algorithm

--- week4/paths_in_graphs_starter_files_2/dijkstra/dijkstra.py
+++ week4/paths_in_graphs_starter_files_2/dijkstra/dijkstra.py
@@ -3,9 +3,29 @@
 import sys
 import queue
 
+def find_min(nodes):
+    left_nodes = (node for node in nodes if node["visited"] is False)
+    minimum = min(left_nodes, key=lambda node: node["distance"], default=False)
+    return minimum
 
 def distance(adj, cost, s, t):
     #write your code here
+    nodes = list({"visited": False, "neighbours": neighbours, "cost": cost[index], "distance": 10000000, "prev": False, "index": index} for index, neighbours in enumerate(adj))
+    nodes[s]["distance"] = 0
+    for min_node in iter(lambda: find_min(nodes), False):
+        min_node["visited"] = True
+        for index, neighbour_index in enumerate(min_node["neighbours"]):
+            neighbour = nodes[neighbour_index]
+            extended_distance = min_node["distance"] + min_node["cost"][index]
+            if neighbour["distance"] > extended_distance:
+                neighbour["distance"] = extended_distance
+                min_node["prev"] = min_node["index"]
+        if min_node["index"] == t:
+            if min_node["distance"] == 10000000:
+                return -1
+            else:
+                return min_node["distance"]
+
     return -1
 
 
